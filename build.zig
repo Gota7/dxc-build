f9da29e3f0065e8254dd2c9152e42943b3945503
Gota7 <gotaseven@gmail.com>
2026-03-12 21:39:07 -0400

    Use python3

--- build.zig
+++ build.zig
@@ -1004,7 +1004,7 @@ fn spvToolsLanguageHeader(b: *std.Build, output: std.Build.LazyPath, grammar_fil
 
 // TODO: Use non-system python (allyourcodebase/cpython?, but it's not updated yet)
 fn pythonCommand(b: *std.Build) !*std.Build.Step.Run {
-    const cmd = b.addSystemCommand(&.{"python"});
+    const cmd = b.addSystemCommand(&.{"python3"});
     return cmd;
 }
 
